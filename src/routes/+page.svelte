184717bf9413c4a92a30a93046ce3c460f27db88
Petras Surna <petras.surna@yart.com.au>
2024-11-01 17:17:24 +1100

    1
    1

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<h1>Welcome to SvelteKit!</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+
+
+Your identification has been saved in C:\Users\petra/.ssh/id_ed25519
+Your public key has been saved in C:\Users\petra/.ssh/id_ed25519.pub
+The key fingerprint is:
+SHA256:J96u5P9Hoa5JwD8LwjPRQyG/Dxqdg1CqliVeG/0B1Qk petras.surna@yart.com.au
+The key's randomart image is:
